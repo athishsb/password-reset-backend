3598d3b43db3d0380ea06874bedf9f1ae1c63aca
athishsb <athishbalajisb22@gmail.com>
2026-05-15 03:40:08 +0530

    resolved email sending issue

--- Utility/sendMail.js
+++ Utility/sendMail.js
@@ -1,34 +1,35 @@
-import { Resend } from "resend";
-
-const resend = new Resend(process.env.RESEND_API_KEY);
+import nodemailer from "nodemailer";
 
 // This function takes email,userId,token
-// uses resend npm to send password reset email to the user requesting password reset
+// uses nodemailer npm to send password reset email to the user requesting password reset
 export const sendPasswordResetMail = async (email, userId, token) => {
-  try {
+  const link = `https://pswd-reset.netlify.app/reset-password/${userId}/${token}`;
 
-    const link = `https://pswd-reset.netlify.app/reset-password/${userId}/${token}`;
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    secure: false,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    },
+  });
 
-    const mailOptions = {
-      from: "onboarding@resend.dev",
-      to: email,
-      subject: "Reset Password for your app",
-      text: `Hello,Follow this link to reset your app password for your ${email} account.
+  const mailOptions = {
+    to: email,
+    from: `${process.env.EMAIL_FROM_NAME} <${process.env.EMAIL_FROM_ADDRESS}>`,
+    subject: "Reset Password for your app",
+    text: `Hello,Follow this link to reset your app password for your ${email} account.
         ${link}.If you didn\\'t ask to reset your password, you can ignore this email.
         Thanks,
         Your app Admin`,
-      html: `<h4>Reset your Password</h4><br>
+    html: `<h4>Reset your Password</h4><br>
         <p>Hello , Follow this link to reset your app password for your ${email} account.This link is valid for 10 minutes.</p>
         ${link}
         <p>If you didn\'t ask to reset your password, you can ignore this email.</p>
         <p>Thanks,</p>
         <p>Your app Admin</p>
         `,
-    };
-
-    return await resend.emails.send(mailOptions);
-  } catch (error) {
-    console.error("MAIL ERROR:", error);
-    throw error;
-  }
+  };
+  return await transporter.sendMail(mailOptions);
 };
